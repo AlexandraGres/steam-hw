d7f786d81cd499cc6dcffffa25f2d3732a7f2ee0
Oleksandra Hresniuk <meonis89@gmail.com>
2021-08-30 16:29:18 +0300

    fixed refresh page error

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express()
 
 app.use(express.static(__dirname + '/dist/steam-hw'))
 
-app.get('', function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/steam-hw/index.html'))
 })
 
